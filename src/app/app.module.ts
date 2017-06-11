d7910e298805f806bf4981b6d90c18dc4f1998ee
Alexander Ivanov <pravbeseda@yandex.ru>
2017-06-11 13:31:23 +0300

    Маршрутизация и заготовки новых компонент

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,29 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {Routes, RouterModule} from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { TaskEditComponent } from './task-edit/task-edit.component';
+import { NotFoundComponent } from './not-found/not-found.component';
+import { TasksListComponent } from './tasks-list/tasks-list.component';
+
+// Маршруты
+const appRoutes: Routes = [
+    { path: '', component: TasksListComponent},
+    { path: 'edit', component: TaskEditComponent},
+    { path: '**', component: NotFoundComponent }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TaskEditComponent,
+    NotFoundComponent,
+    TasksListComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
